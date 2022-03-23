30eb81cf9e30c48623e5e773daf7b66cdfb18adb
tommyblog <kyuzunekun2003@gmai.com>
2022-03-23 12:50:46 +0700

    tommy

---
+++ setting/rules.js
@@ -0,0 +1,22 @@
+/*Kalian bisa ganti ini rulesnya
+ingat !! yang ${botname} jangan di ganti
+biarin aja sans ntar bakalan keluar nama bot luh kok
+*/
+const rules = (prefix, botname) => { 
+return `*_RULES BOT_*
+
+1. Jangan spam bot. 
+Sanksi: *GUA BLOK AJG*
+
+2. Jangan telepon bot.
+Sanksi: *GUA BLOK TAPI KLO CEWE GPP*
+
+3. Jangan memnghina/menghujat bot.
+Sanksi: *GUA BLOK NOMER LU + WAR SANTED*
+
+
+Jika sudah dipahami rules-nya, silakan ketik *${prefix}menu* untuk memulai!
+
+──「 *${botname}* 」──`
+}
+exports.rules = rules
